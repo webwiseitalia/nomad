de2cee5f8c87560f52b332e862bae2c2a51f3e67
webwise-studio <info@webwiseitalia.com>
2025-11-19 13:36:29 +0000

    Updates

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,13 @@
+import nomadbackhero from '../assets/nomadbackhero.png'
+
 export default function Home() {
   return (
-    <section className="w-full h-screen bg-white">
-      {/* Sezione Home - 1920x1080 */}
+    <section className="w-full h-screen bg-white relative">
+      <img
+        src={nomadbackhero}
+        alt="Background"
+        className="w-full h-full object-cover"
+      />
     </section>
   )
 }
